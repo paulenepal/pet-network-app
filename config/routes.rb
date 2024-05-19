e2d85b3c0c954885e0a0d4f81afc0d7b5855c5b0
Paulene Pallan <paulenepallan@gmail.com>
2024-05-19 23:14:59 +0800

    chore: reconfigure routes for devise's user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'registrations'
+    registrations: 'users/registrations'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
