ad320277d5231cadca672706f1a096767e75ed60
Paulene Pallan <paulenepallan@gmail.com>
2024-05-19 23:13:41 +0800

    refactor: moved to devise registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :location, :role,
-      adopter_attributes: [:preference, :first_name, :last_name],
-      shelter_attributes: [:name, :address, :phone_number, :website, :description]
-    ])
-  end
-
 end
