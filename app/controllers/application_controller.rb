f580f97dd39c373dfe3b25449aa4bcbaeb3e98d9
msegfilph <menard.seguenza@gmail.com>
2024-05-21 08:55:17 +0800

    chore: merge latest admin branch into latest branch 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
   
     protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :location, :role,
-        adopter_attributes: [:preference, :first_name, :last_name],
-        shelter_attributes: [:name, :address, :phone_number, :website, :description]
-      ])
-    end
   
     def ensure_admin
       unless current_user&.admin?
