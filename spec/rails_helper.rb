dd069e76763ea8df292dccff0b3003a239eb378e
msegfilph <menard.seguenza@gmail.com>
2024-05-25 09:57:42 +0800

    Test: added sign_in method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Warden::Test::Helpers
+
+  config.before :suite do
+    Warden.test_mode!
+  end
+
+  config.after :each do
+    Warden.test_reset!
+  end
 end
--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Dashboards", type: :request do
+  before do
+    @admin = User.create!(
+      email: "admin@example.com", 
+      password: "password", 
+      role: :admin, 
+      username: "adminuser", 
+      location: "Admin City"
+    )
+  end
+
   describe "GET /index" do
     it "returns http success" do
+      sign_in @admin
+
+      expect(@admin.admin?).to be_truthy
+
       get "/admin/dashboard/index"
+      
+      if response.status == 302
+        puts "Redirected to: #{response.headers['Location']}"
+      end
+
       expect(response).to have_http_status(:success)
     end
   end
-
 end
