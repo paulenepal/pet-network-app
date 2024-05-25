9721f5b9b6395d35090e34a8081b6cd89d9f329e
Paulene Pallan <paulenepallan@gmail.com>
2024-05-25 21:17:20 +0800

    feat: added geocoder for pet location

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,12 @@
 class Pet < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode
+
   enum adoption_status: { available: 0, pending: 1, adopted: 2 }
 
   belongs_to :shelter
   has_many :favorites
+  has_many :favorited_by_users, through: :favorites, source: :use
   has_many :adoption_applications
   has_many :pet_comments
 
