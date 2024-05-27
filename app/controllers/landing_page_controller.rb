1ae3ee413c6665299844bbde89e6d338c28762e7
Paulene Pallan <paulenepallan@gmail.com>
2024-05-27 23:15:46 +0800

    feat: added root pages for every user after sign in

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,5 +2,18 @@ class LandingPageController < ApplicationController
   layout 'landing_page'
 
   def index
+    if user_signed_in?
+      if current_user.admin?
+        redirect_to admin_dashboard_path
+      elsif current_user.adopter?
+        redirect_to adopter_dashboard_path
+      elsif current_user.shelter?
+        redirect_to shelter_dashboard_path
+      else
+        render :index
+      end
+    else
+      render :index
+    end
   end
 end
