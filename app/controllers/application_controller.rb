834e978a029a6df0cd51c07ff9f13a9fec5c364f
Paulene Pallan <paulenepallan@gmail.com>
2024-06-05 23:25:17 +0800

    feat: implemented 'rescue_from to handle User::NotAuthorized exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from User::NotAuthorized, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'Access denied.'
+    redirect_to(root_path)
+  end
   
 end
