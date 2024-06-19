588a19e903c54eb949320c65a24cedc8e47877d4
emjeic <marliejohnc.official@gmail.com>
2024-06-19 21:40:41 +0800

    Update javascript file for chat functionality

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "application"
+pin "application", to: "application.js"
 pin "admin/users"
-# pin "sendbird", to: "sendbird.min.js", preload: true
 
